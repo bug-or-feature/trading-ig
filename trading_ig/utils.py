e9fb4d279cbc9b9ef738f5d5ac62874b9dfca4b6
Andy Geach <andy@bugorfeature.net>
2024-02-06 17:07:03 +0000

    throw an exception if IG date str has an unexpected format

--- trading_ig/utils.py
+++ trading_ig/utils.py
@@ -89,10 +89,10 @@ def parse_utc(date_str):
     # 2023-10-19T13:30:33.565 (23)
     # 2023-10-31T15:57:48.39 (22)
     # 2023-10-19T13:30:33 (19)
-    # PROBLEM: Unexpected format for IG date: 2023-10-31T15:57:48.39. Returning now
-
     if len(date_str) >= 19:
         return datetime.datetime.strptime(date_str[:19], "%Y-%m-%dT%H:%M:%S")
+    else:
+        raise Exception(f"Unexpected format for IG date: {date_str}")
 
 
 def conv_to_ms(td):
